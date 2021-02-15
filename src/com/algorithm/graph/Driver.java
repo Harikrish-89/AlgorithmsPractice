6c82870115924cbf0b227c5e13177418f58f2a23
Harikrish-89 <harikrishnansridhar@gmail.com>
2021-02-15 13:00:12 +0000

    better understanding

--- src/com/algorithm/graph/Driver.java
+++ src/com/algorithm/graph/Driver.java
@@ -13,19 +13,14 @@ import java.util.stream.IntStream;
 public class Driver {
 // Practice for Graph with Collections.
 	public static void main(String[] args) {
-		Graph<String> graph = new Graph<String>();
-		graph.addVertex("Bob");
-		graph.addVertex("Alice");
-		graph.addVertex("Mark");
-		graph.addVertex("Rob");
-		graph.addVertex("Maria");
-		graph.addEdge("Bob", "Alice", false);
-		graph.addEdge("Bob", "Rob", false);
-		graph.addEdge("Alice", "Mark", false);
-		graph.addEdge("Rob", "Mark", false);
-		graph.addEdge("Alice", "Maria", false);
-		graph.addEdge("Rob", "Maria", false);
-		System.out.println(graph);
+		/*
+		 * Graph<String> graph = new Graph<String>(); graph.addVertex("Bob");
+		 * graph.addVertex("Alice"); graph.addVertex("Mark"); graph.addVertex("Rob");
+		 * graph.addVertex("Maria"); graph.addEdge("Bob", "Alice", false);
+		 * graph.addEdge("Bob", "Rob", false); graph.addEdge("Alice", "Mark", false);
+		 * graph.addEdge("Rob", "Mark", false); graph.addEdge("Alice", "Maria", false);
+		 * graph.addEdge("Rob", "Maria", false); System.out.println(graph);
+		 */
 		int[][] ladders = new int[][] { { 32, 62 }, { 42, 68 }, { 12, 98 } };
 		int[][] snakes = new int[][] { { 95, 13 }, { 97, 25 }, { 93, 37 }, { 79, 27 }, { 75, 19 }, { 49, 47 },
 				{ 67, 17 } };
@@ -63,13 +58,14 @@ public class Driver {
 		});
 
 		System.out.println(snakeAndLadder);
-		depthFirst(graph, "Bob");
+		depthFirst(snakeAndLadder, 1);
 		Map<Integer, Integer> dist = new HashMap<Integer, Integer>();
 		Map<Integer, Integer> pred = new HashMap<Integer, Integer>();
 		Integer source = 1;
 		Integer dest = 100;
 		// bfsOrShortestPath(graph, source, dest, dist, pred);
 		bfsOrShortestPath(snakeAndLadder, source, dest, dist, pred);
+		System.out.println("Distance:" + dist);
 		System.out.println("short path =>" + dist.get(dest));
 		Integer crawl = dest;
 		ArrayList<Integer> path = new ArrayList<Integer>();
@@ -88,12 +84,12 @@ public class Driver {
 		System.out.println("PathString:" + pathString.toString());
 	}
 
-	public static void depthFirst(Graph<String> g, String vertex) {
-		Set<String> visited = new LinkedHashSet<String>();
-		Stack<String> stack = new Stack<String>();
+	public static <T> void depthFirst(Graph<T> g, T vertex) {
+		Set<T> visited = new LinkedHashSet<T>();
+		Stack<T> stack = new Stack<T>();
 		stack.push(vertex);
 		while (!stack.isEmpty()) {
-			String vert = stack.pop();
+			T vert = stack.pop();
 			if (!visited.contains(vert)) {
 				visited.add(vert);
 				g.getAdjVertices(vert).stream().forEach(adjVerts -> {
@@ -101,7 +97,7 @@ public class Driver {
 				});
 			}
 		}
-		System.out.println(visited);
+		System.out.println("DFS=>" + visited);
 	}
 
 	public static <T> void bfsOrShortestPath(Graph<T> g, T source, T dest, Map<T, Integer> dist, Map<T, T> pred) {
@@ -109,21 +105,18 @@ public class Driver {
 		Queue<T> queue = new LinkedList<T>();
 		dist.put(source, 0);
 		queue.add(source);
+		visited.add(source);
 		while (!queue.isEmpty()) {
 			T vert = queue.poll();
-			visited.add(vert);
-			for (T adjVert : g.getAdjVertices(vert)) {
+			g.getAdjVertices(vert).stream().forEach(adjVert -> {
 				if (!visited.contains(adjVert)) {
 					visited.add(adjVert);
+					queue.add(adjVert);
 					dist.put(adjVert, dist.get(vert) + 1);
 					pred.put(adjVert, vert);
-					queue.add(adjVert);
-					if (adjVert.equals(dest)) {
-						return;
-					}
 				}
-			}
+			});
 		}
-		System.out.println(visited);
+		System.out.println("BFS=>" + visited);
 	}
 }
